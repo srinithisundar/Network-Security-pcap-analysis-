a4e6936b6a4d6bbf8f0c73899e9c8682df355ca5
Srinithi Meenakshi Sundaram <srinithirenuka@gmail.com>
2025-02-21 00:12:59 -0500

    Update pcap_analysis.sh

--- pcap_analysis.sh
+++ pcap_analysis.sh
@@ -5,7 +5,7 @@
 # Total number of packets in the pcap file 
 tcpdump -tttt -r tcpdump_challenge.pcap --count
 
-# Total number of ICMP packets in the pcap file packet capture
+# Total number of ICMP packets in the pcap file
 tcpdump -tttt -r tcpdump_challenge.pcap -n icmp --count
 
 # ASN of the destination IP address that the endpoint was pinging
@@ -27,25 +27,25 @@ tcpdump -r tcpdump_challenge.pcap | cut -d " " -f 4-9 | grep -E 'GET|POST'
 # Get the host information in th ASCII format
 tcpdump -r tcpdump_challenge.pcap host 10.0.2.10 -A 
 
-# Credentials found in the pcap file within the payloads of the HTTP packets
+# Find credentials within HTTP payloads
 tcpdump -tttt -r tcpdump_challenge.pcap host 10.0.2.10 -A | grep -i 'user\|pass\|login'
 
 # Filtering just the password
 tcpdump -tttt -r tcpdump_challenge.pcap host 10.0.2.10 -A | grep -i 'pass'
 
-# TCP port which are most used and well-known in the capture other than http port 80
+# Identify commonly used TCP ports (excluding HTTP)
 tcpdump -tttt -r tcpdump_challenge.pcap -n tcp and not port 80 | sort
 
-# Valid credentials the endpoints used to access the file sharing server for sharing a file
+# Valid credentials used to access the FTP server
 tcpdump -tttt -r tcpdump_challenge.pcap host 10.0.2.10 and -i any port 21 -A | grep -i 'user\|pass'
 
-# Name of file retrieved from the server
+# Retrieve the name of a file downloaded from the server
 tcpdump -r tcpdump_challenge.pcap -i any port 21 -A | grep -i 'RETR'
 
-# Unique User-agent found in the user list of http requests
+# Identify unique User-Agent strings
 tcpdump -tttt -r tcpdump_challenge.pcap host 10.0.2.10 -A | grep -i 'user'
 
 #Use google and find the malware type of that agent, I used found User-agent and found its Lumma-infostealer. Refer to 
 
-#URL that the endpoint tried to connect to the User-Agent: TeslaBrowser
+#Extract URL accessed by TeslaBrowser User-Agent
 tcpdump -tttt -r tcpdump_challenge.pcap host 10.0.2.10 -A | grep -i "User-Agent: .*TeslaBrowser/5.5" -A 50
